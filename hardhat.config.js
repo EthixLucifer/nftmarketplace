1587eca7b25eb16bb37ba9aaccb04631d0400430
EthixLucifer <luciferethix@gmail.com>
2022-09-08 17:21:27 +0530

    installed libs

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,20 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  
+  networks: {
+    hardhat: {
+      
+      chainId: 1337,
+    }
+  }
 };
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
